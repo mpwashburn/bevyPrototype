4e7bbaee616d6915d25a96bd19573e5129759729
mpwashburn <washburn.michael@gmail.com>
2019-04-05 04:33:58 -0700

    create login and signup buttons on welcome screen

--- src/common/BtnLong.js
+++ src/common/BtnLong.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
+const BtnLong = () => {
+  return (
+    <View>
+      <Button 
+      onPress={()=>{this.props.navigation.navigate('Insert')}} 
+      title=''
+      color=''
+      accessibilityLabel=''
+      />
+    </View>
+  );
+}
+
+export default BtnLong;
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,14 +1,68 @@
 import React, { Component  } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 export default class Welcome extends Component {
-  state = {  }
   render() {
+    const {btnContainer, btnLogin, btnSignup, btnLongText, logoContainer, logoText} = styles;
     return (
-      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-        <Text>Welcom Screen</Text>
-        <Button title='Login' onPress={() => {this.props.navigation.navigate('Discover')}} />
+      <View style={{flex:1}}>
+        <View style={logoContainer}>
+          <Text style={logoText}>Bevy</Text>
+        </View>
+        <View style={btnContainer}>
+          <TouchableOpacity style={btnLogin} title='Login' onPress={() => {this.props.navigation.navigate('Discover')}}>
+          <Text style={btnLongText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={btnSignup} title='Login' onPress={() => {this.props.navigation.navigate('#')}}>
+          <Text style={btnLongText}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+
+const styles = {
+  logoContainer:{
+    justifyContent:'center',
+    alignSelf:'center',
+    marginTop:175,
+    borderColor:'#ff5714',
+    borderWidth:5,
+    borderRadius: 40,
+    height: 130,
+    width: 130,
+  },
+  logoText:{
+    alignSelf:'center',
+    fontSize: 36,
+    padding:20,
+    fontWeight:'600',
+  },
+  btnContainer: {
+    justifyContent:'center',
+    alignSelf:'center',
+    marginTop:100
+  },
+  btnLogin: {
+    width:300,
+    height:40,
+    borderRadius:25,
+    backgroundColor:'#6eeb83',
+    marginTop:5,
+    marginBottom:5
+  },
+  btnSignup: {
+    width:300,
+    height:40,
+    borderRadius:25,
+    backgroundColor:'#1be7ff',
+    marginTop:5,
+    marginBottom:5
+  },
+  btnLongText: {
+    alignSelf:'center',
+    paddingTop:10,
+    paddingBottom:10
+  }
+};
