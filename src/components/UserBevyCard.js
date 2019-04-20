78d6b7793943a1ecc7826df016c39a955640bb19
mpwashburn <washburn.michael@gmail.com>
2019-04-20 14:36:44 -0700

    add profile and thumbnail images to template. Added cover image.

--- src/components/UserBevyCard.js
+++ src/components/UserBevyCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from '../components/common/Card';
 import CardSection from '../components/common/CardSection'
 
@@ -8,20 +8,25 @@ const UserBevyCardCover = (props) => {
     <Card>
       <CardSection>
         <View style={{flexDirection:'column'}}>
-        <View><Text>Image should be set behind title</Text></View>
+        <View>
+          <Image 
+            style={{width:353, height:150}} 
+            source={{uri:'https://via.placeholder.com/400'}} 
+            />
+          </View>
         <View><Text>Trip Title</Text></View>
         </View>
       </CardSection>
       <CardSection>
-        <View>
-        <Text>Profile Pic</Text>
-        <Text>Username</Text>
+        <View style={{flex:1, paddingLeft:10}}>
+        <Image style={{width:50, height:50, borderRadius:25}} source={{uri:'https://via.placeholder.com/50'}} />
+        <Text style={{fontSize:10}}>Username</Text>
         </View>
-        <View>
-        <Text>Pic 1</Text>
-        <Text>Pic 2</Text>
-        <Text>Pic 3</Text>
-        <Text>Pic 4</Text>
+        <View style={{flex:3, flexDirection:'row', justifyContent:'space-around', alignItems:'center'}}>
+        <Image style={{width:50, height:50}} source={{uri:'https://via.placeholder.com/50'}} />
+        <Image style={{width:50, height:50}} source={{uri:'https://via.placeholder.com/50'}} />
+        <Image style={{width:50, height:50}} source={{uri:'https://via.placeholder.com/50'}} />
+        <Image style={{width:50, height:50}} source={{uri:'https://via.placeholder.com/50'}} />
         </View>
       </CardSection>
     </Card>
